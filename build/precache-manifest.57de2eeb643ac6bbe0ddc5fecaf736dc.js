8d90d9d3ed2e139723af20c367ea0051c5ba441c
shin donggyu <donggyu9410@gmail.com>
2020-02-21 03:43:09 +0900

    build file

---
+++ build/precache-manifest.57de2eeb643ac6bbe0ddc5fecaf736dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6faa0f219999bb1d9b01b7f1d770e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9575defa90e0502537a",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "9d665112652a1fad13ab",
+    "url": "/static/css/main.49cb7a9a.chunk.css"
+  },
+  {
+    "revision": "e9575defa90e0502537a",
+    "url": "/static/js/2.dfb7a5a7.chunk.js"
+  },
+  {
+    "revision": "cecea7edf82dae13b8ebd17a69225951",
+    "url": "/static/js/2.dfb7a5a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d665112652a1fad13ab",
+    "url": "/static/js/main.a51456d4.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
